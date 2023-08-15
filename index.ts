a0c1a6c8efd925deada645c5b453da6bee14dfae
MihirDesh <mihirdesh23@gmail.com>
2023-08-15 23:18:33 +0530

    auth middleware updated

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
 import express from "express";
 import queryDatabase from "./database/connection";
-
+import session from "express-session";
+import passport from 'passport';
 const app = express();
 const port = process.env.PORT || 3000;
 
 import JudgeRouter from "./routes/judge";
 import TeamRouter from "./routes/teams";
 import ParameterRouter from "./routes/parameters";
+
+app.set('view engine', 'ejs'); // Set 'ejs' as the view engine
+
+
+app.use(session({
+  secret: 'your-session-secret',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 
