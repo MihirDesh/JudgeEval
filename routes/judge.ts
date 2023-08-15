86cac8620698fc3f971b53318f1ca3db4fa1ba61
MihirDesh <mihirdesh23@gmail.com>
2023-08-15 23:18:57 +0530

    auth middleware updated

--- routes/judge.ts
+++ routes/judge.ts
@@ -1,4 +1,7 @@
+import { Request, Response } from "express";
 const express = require("express");
+
+import { fetchuser } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
@@ -9,6 +12,12 @@ import {
   updateJudge,
   deleteJudge,
 } from "../controllers/judge";
+
+
+// Route to display the login form
+router.get('/login', (req : Request, res : Response) => {
+  res.sendFile('login.html', { root: 'views' });
+});
 
 router.route("/").post(createJudge).get(getAllJudges);
 router.route("/:id").get(getSingleJudge).patch(updateJudge).delete(deleteJudge);
